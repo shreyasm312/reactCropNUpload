8238a5069c3eb30c291a5f2a1ea52a9f2ee50835
shreyasm312 <shreyasm312@gmail.com>
2020-03-28 01:49:48 +0530

    displaying crop image

--- src/views/components/canvas/canvas.js
+++ src/views/components/canvas/canvas.js
@@ -19,8 +19,78 @@ export class Canvas extends Component {
     this.ctx = this.canvas.getContext('2d');
     this.ctx.strokeStyle = this.props.strokeStyle;
     this.ctx.lineWidth = this.props.lineWidth;
-    // this.addMouseEvents();
+
+    this.addMouseEvents();
   }
+  componentDidUpdate(prevProps, prevState) {
+    if (this.props.imageSRC !== prevProps.imageSRC) {
+      let img = new Image();
+      img.onload = () => {
+        this.ctx.drawImage(img, 33, 71, 104, 124, 21, 20, 87, 104);
+      };
+      img.src = this.props.imageSRC;
+    }
+  }
+  componentWillUnmount() {
+    this.removeMouseEvents();
+  }
+  addMouseEvents() {
+    document.addEventListener('mousedown', this.onMouseDown, false);
+    document.addEventListener('mousemove', this.onMouseMove, false);
+    document.addEventListener('mouseup', this.onMouseUp, false);
+  }
+  removeMouseEvents() {
+    document.removeEventListener('mousedown', this.onMouseDown, false);
+    document.removeEventListener('mousemove', this.onMouseMove, false);
+    document.removeEventListener('mouseup', this.onMouseUp, false);
+  }
+
+  onMouseDown = e => {
+    this.isDrag = true;
+    this.curX = this.startX = e.offsetX;
+    this.curY = this.startY = e.offsetY;
+    // requestAnimationFrame(this.updateCanvas)
+  };
+
+  onMouseMove = e => {
+    if (!this.isDrag) return;
+    this.curX = e.offsetX;
+    this.curY = e.offsetY;
+    this.isDirty = true;
+  };
+
+  onMouseUp = e => {
+    this.isDrag = false;
+    this.isDirty = true;
+    const rect = {
+      x: Math.min(this.startX, this.curX),
+      y: Math.min(this.startY, this.curY),
+      w: Math.abs(e.offsetX - this.startX),
+      h: Math.abs(e.offsetY - this.startY)
+    };
+    this.props.onSelected(rect);
+  };
+
+  // updateCanvas = () => {
+  //   if (this.isDrag) {
+  //     requestAnimationFrame(this.updateCanvas);
+  //   }
+  //   if (!this.isDirty) {
+  //     return;
+  //   }
+  //   this.ctx.clearRect(0, 0, this.props.width, this.props.height);
+  //   if (this.isDrag) {
+  //     const rect = {
+  //       x: this.startX,
+  //       y: this.startY,
+  //       w: this.curX - this.startX,
+  //       h: this.curY - this.startY
+  //     };
+  //     this.ctx.strokeRect(rect.x, rect.y, rect.w, rect.h);
+  //   }
+  //   this.isDirty = false;
+  // };
+
   render() {
     return (
       <canvas
--- src/views/pages/upload.js
+++ src/views/pages/upload.js
@@ -45,7 +45,7 @@ export class Upload extends Component {
             </div>
             <div className="h-6">
               <h1>Selected Image</h1>
-              <Canvas />
+              <Canvas imageSRC={this.state.imageSRC} />
               {/* <img src={this.state.imageSRC} alt=""></img> */}
             </div>
           </div>
