a13b1670b00e3af37d3bfa24a553ca95bca63f12
shreyasm312 <shreyasm312@gmail.com>
2020-03-30 18:38:39 +0530

    added help text for cropping

--- src/views/components/crop.js
+++ src/views/components/crop.js
@@ -64,7 +64,11 @@ export class Crop extends Component {
       <div className="flex flex-col items-center justify-center">
         {!this.state.isInValidImage && this.state.imageRef !== null && (
           <div className="my-4" style={{ minHeight: 1024, minWidth: 1024 }}>
-            <h1>Crop your image</h1>
+            <h1 className="text-xl">Crop your image</h1>
+            <h1 className="text-gray-700">
+              Please click and drag on the crop square to find the selection you want to crop and
+              release the mouse click to crop.
+            </h1>
             <Canvas
               imageRef={this.props.imageData.imageRef}
               imageSRC={this.props.imageData.imageSRC}
